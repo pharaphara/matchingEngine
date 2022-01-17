7f9fc185fa0a40a3c2a088949007e2a0f3d2c65e
alex <alexander.deltour@gmail.com>
2022-01-17 11:09:22 +0100

    sans mdp2

--- src/main/java/fr/eql/matchingEngine/security/WebSecurityConfig.java
+++ src/main/java/fr/eql/matchingEngine/security/WebSecurityConfig.java
@@ -13,42 +13,42 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-@Configuration
-@EnableWebSecurity
-
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-
-	@Value("${app.username}")
-	private String username;
-	@Value("${app.password}")
-	private String password;
-	
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-		//log.info("username {}, ",username);
-        
-		http.sessionManagement().sessionCreationPolicy(
-                SessionCreationPolicy.STATELESS)
-		.and()
-        .authorizeRequests()
-        .antMatchers("/**").authenticated() // These urls are allowed by any authenticated user
-        .and()
-        .httpBasic();
-        http.csrf().disable();
-    }
-
-	@Bean
-    public UserDetailsService userDetailsService() {
-		//log.info("username {}, ",username);
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        String encodedPassword = passwordEncoder().encode(password);
-        manager.createUser(User.withUsername(username).password(encodedPassword).roles("USER").build());
-        return manager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//
+//@Configuration
+//@EnableWebSecurity
+//
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+//
+//	@Value("${app.username}")
+//	private String username;
+//	@Value("${app.password}")
+//	private String password;
+//	
+//	@Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//		//log.info("username {}, ",username);
+//        
+//		http.sessionManagement().sessionCreationPolicy(
+//                SessionCreationPolicy.STATELESS)
+//		.and()
+//        .authorizeRequests()
+//        .antMatchers("/**").authenticated() // These urls are allowed by any authenticated user
+//        .and()
+//        .httpBasic();
+//        http.csrf().disable();
+//    }
+//
+//	@Bean
+//    public UserDetailsService userDetailsService() {
+//		//log.info("username {}, ",username);
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        String encodedPassword = passwordEncoder().encode(password);
+//        manager.createUser(User.withUsername(username).password(encodedPassword).roles("USER").build());
+//        return manager;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
