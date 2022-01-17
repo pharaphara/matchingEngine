185077fd979ba25fe5eded6d738bbf2998342405
Alexandre DELTOUR <alexandre.deltour@geodis.com>
2022-01-17 11:09:22 +0100

    sans mdp2

--- src/main/java/fr/eql/matchingEngine/dao/PriceRepo.java
+++ src/main/java/fr/eql/matchingEngine/dao/PriceRepo.java
@@ -1,14 +1,18 @@
 package fr.eql.matchingEngine.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import fr.eql.matchingEngine.dto.constant.TradingPair;
 import fr.eql.matchingEngine.dto.model.Price;
 
 public interface PriceRepo extends JpaRepository<Price, Integer> {
-//	
-//	@Query("SELECT p FROM RuleVo r where r.name = :name") 
-//    List<Long> findIdByName(@Param("name") String name);
+	
+//	@Query("SELECT p FROM Price p where p.pair = :name") 
+//    List<Price> findIdByName(@Param("pair") TradingPair pair);
 
 
 }
