28de1ef5b70a3c21e21b477f2449bccb163cff9a
Alexandre DELTOUR <alexandre.deltour@geodis.com>
2022-01-18 14:48:18 +0100

    ajout test

--- target/matchingEngine-matchingEngine/WEB-INF/classes/data.sql
+++ target/matchingEngine-matchingEngine/WEB-INF/classes/data.sql
@@ -1,17 +1,27 @@
+INSERT INTO `currency` VALUES 
+('BNB','Binance Coin',166801148),
+('BTC','Bitcoin',18932487),
+('DOT','Polkadot',987579315),
+('ETH','Ethereum',119201202),
+('USDT','Tether',78399569301),
+('XRP','Ripple',47663117635);
+
+
 INSERT INTO ordre (amount,creation_date,currency_pair,filledamount,limit_price,order_type,status,user)
-VALUES('1', '2022-01-14 08:25:28.836000', 'USDT_EUR', '0', '24', 'BID', 'NEW', 'ALEX');
+VALUES('1', '2022-01-14 08:25:28.836000', 'BTC_EUR', '0', '24', 'BID', 'NEW', 'bigbux@eqlexchange.io');
 INSERT INTO ordre (amount,creation_date,currency_pair,filledamount,limit_price,order_type,status,user)
-VALUES('1', '2022-01-14 08:25:28.836000', 'USDT_EUR', '0', '23', 'BID', 'NEW', 'ALEX');
+VALUES('1', '2022-01-14 08:25:28.836000', 'BTC_EUR', '0', '23', 'BID', 'NEW', 'bigbux@eqlexchange.io');
 INSERT INTO ordre (amount,creation_date,currency_pair,filledamount,limit_price,order_type,status,user)
-VALUES('1', '2022-01-14 08:25:28.836000', 'USDT_EUR', '0', '22', 'BID', 'NEW', 'ALEX');
+VALUES('1', '2022-01-14 08:25:28.836000', 'BTC_EUR', '0', '22', 'BID', 'NEW', 'alain.musque@yahoo.fr');
 INSERT INTO ordre (amount,creation_date,currency_pair,filledamount,limit_price,order_type,status,user)
-VALUES('1', '2022-01-14 08:25:28.836000', 'USDT_EUR', '0', '21', 'BID', 'NEW', 'ALEX');
+VALUES('1', '2022-01-14 08:25:28.836000', 'BTC_EUR', '0', '21', 'BID', 'NEW', 'alain.musque@yahoo.fr');
 INSERT INTO ordre (amount,creation_date,currency_pair,filledamount,limit_price,order_type,status,user)
-VALUES('1', '2022-01-14 08:25:28.836000', 'USDT_EUR', '0', '25', 'ASK', 'NEW', 'string');
+VALUES('1', '2022-01-14 08:25:28.836000', 'BTC_EUR', '0', '25', 'ASK', 'NEW', 'douilledu13@yopmail.com');
 INSERT INTO ordre (amount,creation_date,currency_pair,filledamount,limit_price,order_type,status,user)
-VALUES('1', '2022-01-14 08:25:28.836000', 'USDT_EUR', '0', '26', 'ASK', 'NEW', 'string');
+VALUES('1', '2022-01-14 08:25:28.836000', 'BTC_EUR', '0', '26', 'ASK', 'NEW', 'douilledu13@yopmail.com');
 INSERT INTO ordre (amount,creation_date,currency_pair,filledamount,limit_price,order_type,status,user)
-VALUES('1', '2022-01-14 08:25:28.836000', 'USDT_EUR', '0', '27', 'ASK', 'NEW', 'string');
+VALUES('1', '2022-01-14 08:25:28.836000', 'BTC_EUR', '0', '27', 'ASK', 'NEW', 'pouchard11@numericable.fr');
 INSERT INTO ordre (amount,creation_date,currency_pair,filledamount,limit_price,order_type,status,user)
-VALUES('1', '2022-01-14 08:25:28.836000', 'USDT_EUR', '0', '28', 'ASK', 'NEW', 'string');
+VALUES('1', '2022-01-14 08:25:28.836000', 'BTC_EUR', '0', '28', 'ASK', 'NEW', 'pouchard11@numericable.fr');
+
 
