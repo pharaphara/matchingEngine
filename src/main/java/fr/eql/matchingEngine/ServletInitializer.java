c64a9442504b3391783191c1fd0cf9798f0aecb4
Alexandre DELTOUR <alexandre.deltour@geodis.com>
2022-01-19 09:48:40 +0100

    exclusion tomcat3

--- src/main/java/fr/eql/matchingEngine/ServletInitializer.java
+++ src/main/java/fr/eql/matchingEngine/ServletInitializer.java
@@ -1,13 +1,13 @@
-//package fr.eql.matchingEngine;
-//
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//
-//public class ServletInitializer extends SpringBootServletInitializer {
-//
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(MatchingEngineApplication.class);
-//	}
-//
-//}
+package fr.eql.matchingEngine;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MatchingEngineApplication.class);
+	}
+
+}
